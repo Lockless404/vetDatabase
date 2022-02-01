dd05c22c02d3cd8f46a5a13307eb9eb647ee71af
Hamza <hamzatarar8@gmail.com>
2022-02-01 12:10:53 +0200

    Updated table, added items, and manipulated data using transactions.

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 /*Queries that provide answers to the questions from all projects.*/
 
-SELECT * FROM animals where name LIKE '%mon%';
+SELECT * FROM animals where name LIKE '%mon';
 SELECT name FROM animals where date_of_birth BETWEEN '2016-1-1' AND '2019-12-1';
 SELECT name FROM animals where neutered = true and escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name IN ('Agumon', 'Pikachu');
